a9260b968e4fafd1602a3041e6cb11799bcf11c9
Harry Greentree <harrygreentree@icloud.com>
2021-07-17 02:33:25 +0100

    Base mod commit.

---
+++ src/main/java/io/github/TheBlackSquidward/squidwardapi/SquidwardAPI.java
@@ -0,0 +1,18 @@
+package io.github.TheBlackSquidward.squidwardapi;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(SquidwardAPI.MOD_ID)
+public class SquidwardAPI {
+
+    public static final String MOD_ID = "squidwardapi";
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public SquidwardAPI() {
+
+    }
+
+}
